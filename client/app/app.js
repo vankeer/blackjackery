d7b16186bc443952c9c1c47fd69cced537532473
Laurens Van Keer <laurens@snoozon.com>
2016-01-23 15:35:06 +0100

    initial commit (angular-fullstack generated files)

---
+++ client/app/app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+angular.module('blackjackeryApp', [
+  'blackjackeryApp.constants',
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'ui.router',
+  'ui.bootstrap'
+])
+  .config(function($urlRouterProvider, $locationProvider) {
+    $urlRouterProvider
+      .otherwise('/');
+
+    $locationProvider.html5Mode(true);
+  });

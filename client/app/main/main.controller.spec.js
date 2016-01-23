31612725d650413713a018c8e1f3db05f98f5651
Laurens Van Keer <laurens@snoozon.com>
2016-01-23 16:16:42 +0100

    basic unit test of the main controller

--- client/app/main/main.controller.spec.js
+++ client/app/main/main.controller.spec.js
@@ -12,14 +12,26 @@ describe('Controller: MainController', function() {
   var $httpBackend;
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function(_$httpBackend_, $controller, $rootScope, $state) {
+  beforeEach(inject(function(
+    _$httpBackend_,
+    $controller,
+    $rootScope,
+    $state,
+    Game // TODO replace with mock service
+  ) {
     $httpBackend = _$httpBackend_;
 
     scope = $rootScope.$new();
     state = $state;
     MainController = $controller('MainController', {
-      $scope: scope
+      $scope: scope,
+      Game: Game
     });
   }));
+
+  it('should have hit and stick actions', function() {
+    expect(MainController.hit).not.toBeUndefined();
+    expect(MainController.stick).not.toBeUndefined();
+  });
 
 });
